eff1c2987a7a309d8ef0add9808675aabbdd6a9d
Vinod Rathod <vinod.k.rathod93@gmail.com>
2015-01-02 18:30:55 +0530

    Initial Commit

---
+++ Pupus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pupus
+//
+//  Created by Vinod Rathod on 02/01/15.
+//  Copyright (c) 2015 Vinod Rathod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
